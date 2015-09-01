b883cc9005783872a91b6209d773cd0cffe69b16
Matt Williams <mcw8d@ccri.com>
2015-09-01 17:30:29 -0400

    Some work from work.

--- bot.js
+++ bot.js
@@ -1,8 +1,38 @@
 function startBotListener() {
-    var chatContainerNode = $('.hN.so.Fj.Ij').children(':nth-child(2)')[0];
-    var chatObserver = new MutationObserver(function (mutations) {
+    var inputDiv = $('.vE.dQ.editable');
+    var chatContainerNode = $('.hN.so.Fj.Ij').children(':nth-child(2)');
+    var chatContainerObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutationRecord) {
+            for (var i = 0; i < mutationRecord.addedNodes.length; i++) {
+                var addedNode = $(mutationRecord.addedNodes[i]);
+                if (addedNode.hasClass('.tk.Sn')) {
+                    var chatBlock = addedNode.find('.JL');
+                    if (chatBlock.length === 1) {
+                        console.error('New chatter: ' + addedNode.find('Up.pC').find('img').attr('title'));
+                        console.error('New chat: ' + chatBlock.children().children(':last-child').text());
+                        chatBlockObserver.disconnect();
+                        chatBlockObserver.observe(chatBlock.get(0), observerConfig);
+                    }
+                }
+            }
+        });
+    });
+    var chatBlockObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutationRecord) {
+            for (var i = 0; i < mutationRecord.addedNodes.length; i++) {
+                var addedNode = $(mutationRecord.addedNodes[i]);
+                if (addedNode.hasClass('.Mu.SP')) {
+                    console.error('New chat: ' + addedNode.children(':last-child').text());
+                }
+            }
+        });
+    });
+    var observerConfig = {
+        childList: true
+    };
 
-
+    chatContainerObserver.observe(chatContainerNode.get(0), observerConfig);
+    chatBlockObserver.observe(chatContainerNode.children(':last-child').get(0), observerConfig);
 }
 
 window.setTimeout(function () {
